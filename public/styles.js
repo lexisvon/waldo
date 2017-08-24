38f3bb271496f6d505af9f64e0c8648a28c0b7d7
Lexis von Seggern <lexvon2015@gmail.com>
2017-08-24 15:28:42 -0500

    modal pop up when clicking waldo

--- public/styles.js
+++ public/styles.js
@@ -1,9 +1,32 @@
 window.addEventListener('load', function() {
 	var coordinates = document.getElementsByClassName('xy-cord')[0];
-	coordinates.addEventListener('click', function(getPosition){});
-	function getPosition(event) {
-	  var x = event.x;
-	  var y = event.y;
-	  alert("x:" + x + " y:" + y);
-	}
+	coordinates.addEventListener('click', function(event){
+		var x = event.layerX;
+	  	var y = event.layerY;
+	  	var message = document.getElementsByClassName('modal__title')[0];
+	  	if (x >= 580 && x <= 590 && y >= 200 && y <= 225){
+	  		modal.style.display = "block";
+	  		message.innerHTML = "Waldo!";
+	  	}else {
+	  		modal.style.display = "block";
+	  		message.innerHTML = "Nope!";
+	  	}
+	});
+	
+	
+	// Modal 
+	var modal = document.getElementsByClassName('modal')[0];
+	var modalXClose = document.getElementsByClassName('modal__close')[0];
+	modalXClose.addEventListener('click', function(event) {
+		if (event.target == modalXClose) {
+			modal.style.display = "none";
+		}
+	});
+	modal.addEventListener('click', function(event) {
+		if (event.target == modal) {
+			modal.style.display = "none";
+		}
+	});
 });
+
+// send variable x and y to server and the if statement and then send back the results
